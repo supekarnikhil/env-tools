0c368dd1cd06cf1dcb22f9fc91c6fe2c50b103f6
Nikhil Supekar <11524291+supekarnikhil@users.noreply.github.com>
2022-12-24 10:22:14 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "jest", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "import/no-extraneous-dependencies": "off",
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    "no-use-before-define": ["error", { functions: false, classes: true, variables: true }],
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  arrowParens: "always",
+  endOfLine: "lf",
+  printWidth: 100,
+};
